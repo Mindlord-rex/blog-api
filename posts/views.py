ab950be04cdea8ed8ef80e2a2c251769a68d1a37
Manish Kumar <manish123hello@gmail.com>
2024-07-08 21:39:56 +0530

    filters

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer, UserSerializer
@@ -8,6 +8,9 @@ from .serializers import PostSerializer, UserSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
+    search_fields = ['title','body']
+    ordering_fields = ['created_at']
+    filter_backends = (filters.SearchFilter,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
